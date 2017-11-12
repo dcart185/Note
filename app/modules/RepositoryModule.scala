9cb75c2c0a3c83881e56d1535a8e7d51af678708
David Cartier <davidcartier@Davids-MacBook-Pro.local>
2017-11-11 22:47:58 -0500

    filled out the service

--- app/modules/RepositoryModule.scala
+++ app/modules/RepositoryModule.scala
@@ -1,10 +1,12 @@
 package modules
 
 import com.google.inject.AbstractModule
+import repository.notes.{NotesRepository, NotesRepositoryDatabase}
 import repository.person.{PersonRepository, PersonRepositoryDatabase}
 
 class RepositoryModule extends AbstractModule {
   override def configure() = {
     bind(classOf[PersonRepository]).to(classOf[PersonRepositoryDatabase])
+    bind(classOf[NotesRepository]).to(classOf[NotesRepositoryDatabase])
   }
 }
--- app/repository/notes/NotesService.scala
+++ app/repository/notes/NotesService.scala
@@ -1,5 +1,24 @@
 package repository.notes
 
+import models.Note
+
 class NotesService(notesRepository: NotesRepository) {
 
+  def insertNote(note:Note):Note ={
+    val noteId:Long = notesRepository.insertNote(note)
+    note.copy(id = Some(noteId))
+  }
+  def updateNote(note:Note):Boolean ={
+    val updateCount:Long = notesRepository.updateNote(note)
+    if(updateCount == 1) true
+    else false
+  }
+  def deleteNote(id:Long):Boolean ={
+    val updateCount : Long = notesRepository.deleteNote(id)
+    if(updateCount == 1) true
+    else false
+  }
+  def getNote(id:Long):Note ={
+    notesRepository.getNote(id)
+  }
 }
