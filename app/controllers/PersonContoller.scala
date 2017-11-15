5cd13800242fe8464c5d1de242cff32505a6bead
David Cartier <davidcartier@Davids-MacBook-Pro.local>
2017-11-14 22:05:23 -0500

    using futures

--- app/controllers/PersonContoller.scala
+++ app/controllers/PersonContoller.scala
@@ -9,45 +9,57 @@ import play.api.mvc._
 import repository.person.{PersonRepository, PersonService}
 import play.api.libs.functional.syntax._
 
+import scala.concurrent.{ExecutionContext, Future}
+
 
 @Singleton
 class PersonController @Inject()(cc: ControllerComponents, personRepository: PersonRepository)
-  extends AbstractController(cc) {
+                                (implicit ec:ExecutionContext) extends AbstractController(cc) {
 
  val personService : PersonService = new PersonService(personRepository)
 
-  def insertPerson() = Action(parse.json) { implicit request =>
+  def insertPerson() = Action.async(parse.json) { implicit request =>
     val personResult = request.body.validate[Person]
     personResult.fold(
       errors => {
-        BadRequest(Json.obj("status" ->"KO", "message" -> "invalid input"))
+        Future(BadRequest(Json.obj("status" ->"KO", "message" -> "invalid input")))
       },
       person => {
 
         if(person.password.nonEmpty) {
           val hashedPassword: String = BCrypt.hashpw(person.password.get, BCrypt.gensalt())
           val toBeSavedPerson : Person = person.copy(password = Some(hashedPassword))
-          val savedPerson: Person = personService.insertPerson(toBeSavedPerson)
-          Ok(Json.obj("status" -> "OK", "message" -> (s"The user has been saved with id: ${savedPerson.id.get}")))
+          val savedPerson: Future[Person] = Future(personService.insertPerson(toBeSavedPerson))
+          savedPerson.map(person=>{
+            Ok(Json.obj("status" -> "OK", "message" -> (s"The user has been saved with id: ${person.id.get}")))
+          }).recover{
+            case e:Exception=>{
+              InternalServerError(Json.obj("status" ->"KO", "message" -> "invalid input"))
+            }
+          }
         }
         else{
-          BadRequest(Json.obj("status" ->"KO", "message" -> "invalid input"))
+          Future(BadRequest(Json.obj("status" ->"KO", "message" -> "invalid input")))
         }
       }
     )
   }
 
-  def getPerson(id:Long) = Action {
-
-    val personOption : Option[Person] = personService.getPerson(id)
-
-    personOption match{
-      case None =>{
-        NotFound("Person does not exist")
+  def getPerson(id:Long)  = Action.async { implicit request =>
+    val personOptionFuture : Future[Option[Person]] = Future(personService.getPerson(id))
+    personOptionFuture.map(personOption=>{
+      personOption match {
+        case None =>{
+          NotFound("Person does not exist")
+        }
+        case Some(person)=>{
+          val json = Json.toJson(person)
+          Ok(json)
+        }
       }
-      case Some(person)=>{
-        val json = Json.toJson(person)
-        Ok(json)
+    }).recover{
+      case e:Exception=>{
+        InternalServerError(Json.obj("status" ->"KO", "message" -> "invalid input"))
       }
     }
   }
