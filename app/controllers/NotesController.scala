b5b1e7b9bb6947b8ef2b0ea51b68e26a1ca9c43b
David Cartier <davidcartier@Davids-MacBook-Pro.local>
2017-11-23 21:14:57 -0500

    created get note

--- app/controllers/NotesController.scala
+++ app/controllers/NotesController.scala
@@ -1,19 +1,41 @@
 package controllers
 
+import javax.inject.Singleton
+
 import actions.JWTAuthentication
 import com.google.inject.Inject
+import models.Note
+import play.api.libs.json.Json
 import play.api.mvc._
+import repository.notes.{NotesRepository, NotesService}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
+@Singleton
+class NotesController @Inject()(cc: ControllerComponents,jwtAuthentication:JWTAuthentication,
+                                notesRepository: NotesRepository)(implicit ec:ExecutionContext)
+  extends AbstractController(cc){
 
-class NotesController @Inject()(cc: ControllerComponents,jwtAuthentication:JWTAuthentication)
-                               (implicit ec:ExecutionContext) extends AbstractController(cc){
+  val notesService : NotesService = new NotesService(notesRepository)
 
-  def getNote(noteId:Long) = jwtAuthentication{ request =>
+  def getNote(noteId:Long) = jwtAuthentication.async{ request =>
 
-    Ok("it worked")
+    val noteOptionFuture : Future[Option[Note]] = Future(notesService.getNote(noteId))
 
+    noteOptionFuture.map(noteOption=>{
+      noteOption match {
+        case None =>{
+          NotFound("Note does not exist")
+        }
+        case Some(note)=>{
+          val json = Json.toJson(note)
+          Ok(json)
+        }
+      }
+    }).recover{
+      case e:Exception=>{
+        InternalServerError(Json.obj("status" ->"KO", "message" -> "something went wrong"))
+      }
+    }
   }
-
 }
--- app/models/Note.scala
+++ app/models/Note.scala
@@ -1,3 +1,27 @@
 package models
 
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+
 case class Note(id:Option[Long], personId:Long, subject:String, note:String)
+
+object Note {
+  import Note._
+
+  implicit val personReads : Reads[Note] = (
+    (JsPath \ "id").readNullable[Long] and
+      (JsPath \ "personId").read[Long] and
+      (JsPath \ "subject").read[String] and
+      (JsPath \ "note").read[String]
+    )(Note.apply _)
+
+  implicit val noteWrites : Writes[Note] = new Writes[Note] {
+    override def writes(note: Note): JsValue = Json.obj(
+      "id" -> note.id,
+      "personId" -> note.personId,
+      "subject" -> note.subject,
+      "note" -> note.note
+    )
+  }
+}
